695da0b01472afffb4b497fd8e3add0bb113b576
Resma_M <99134470+Resma-M@users.noreply.github.com>
2022-02-17 21:54:44 +0530

    Add files via upload

---
+++ 3_Implementation/Makefile.c
@@ -0,0 +1,9 @@
+#target  : Dependencies
+#<TAB>commands
+
+Build : project_main.c cricket scoreboard.c test_criket scoreboard. unity.c
+        gcc project_main,c cricket.c test_cricket scoreboard.c unity.c -0 cricket scoreboard.out
+Run  : cricket scoreboard.out
+            ./cricket scoreboard.out
+Clean :
+       rm -rf *.0 *.out *.i *.s   
